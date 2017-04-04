db3cbd1944ebe5ba92d51d403bff00f22ad6a58c
SVolkoff <sabina-volkoff@yandex.com>
2017-04-04 17:49:57 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -7,7 +7,6 @@ template <class T> struct Node
 	T key;
 	Node<T>* Left;
 	Node<T>* Right;
-	Node<T>*parent;
 };
 template <class T> class Tree
 {
@@ -17,28 +16,49 @@ private:
 public:
 	Tree();
 	~Tree();
+	void deleteNode(Node<T>* temp);
 	void insert_node(const T&);
 	int get_count()const;
 	void print()const;
 	Node<T>* find_node(const T& val, Node<T>* temp)const;
 	Node<T>*root_()const;
+	void display(Node<T>* temp, unsigned int level)const;
+	void out()const;
 	void reading(const std::string filename); // чтение из файла
 	void output(std::ostream& ost, Node<T>* temp)const; //вывод в консоль
 	void writing(const std::string filename); //запись в файл 
 };
 
-
 template<class T>
 Tree<T>::Tree()
 {
-	root = 0;   
+	root = nullptr;
 	count = 0;
 }
-template <class T>
+template<class T>
 Tree<T>::~Tree()
 {
-	delete root;
+	deleteNode(root);
 }
+template<class T>
+void Tree<T>::deleteNode(Node<T>* temp)
+{
+	if (!temp)
+		return;
+	if (temp->Left)
+	{
+		deleteNode(temp->Left);
+		temp->Left = nullptr;
+	}
+
+	if (temp->Right)
+	{
+		deleteNode(temp->Right);
+		temp->Right = nullptr;
+	}
+	delete temp;
+}
+
 template<class T>
 Node<T>*Tree<T>::root_()const
 {
@@ -94,12 +114,20 @@ template<typename T>
 void Tree<T>::reading(const std::string filename)
 {
 	ifstream fin(filename);
-	T temp;
-	fin >> temp;
-	while (fin)
+	if (root != nullptr)
 	{
-		insert_node(temp);
+		deleteNode(root->Right);
+		deleteNode(root->Left);
+		root = 0;
+	}
+
+	int k;
+	fin >> k;
+	T temp;
+	for (int i = 0; i < k; ++i)
+	{
 		fin >> temp;
+		insert_node(temp);
 	}
 	fin.close();
 }
@@ -112,15 +140,35 @@ template<typename T>
 void Tree<T>::output(ostream& ost, Node<T>* temp)const
 {
 	if (!temp) return;
-		ost << temp->key << " ";
-		output(ost, temp->Left);
-		output(ost, temp->Right);
+	ost << temp->key << " ";
+	output(ost, temp->Left);
+	output(ost, temp->Right);
 }
 
 template<typename T>
 void Tree<T>::writing(const std::string filename)
 {
 	ofstream fout(filename);
+	fout << count << " ";
 	output(fout, root);
 	fout.close();
+}
+
+template<typename T>
+void Tree<T>::display(Node<T>* temp, unsigned int level)const
+{
+
+	if (temp)
+	{
+		display(temp->Left, level + 1);
+		for (unsigned int i = 0; i < level; i++)
+			std::cout << "-";
+		std::cout << temp->key << std::endl;
+		display(temp->Right, level + 1);
+	}
+}
+template<typename T>
+void Tree<T>::out()const
+{
+	display(root, 0);
 }
